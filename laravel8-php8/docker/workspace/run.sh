bcefeffac737104bdf9d71fea35be6012f680cdd
Claudinei Braine <50373192+claubraine@users.noreply.github.com>
2021-12-23 08:19:00 -0300

    Create run.sh

---
+++ laravel8-php8/docker/workspace/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# cp .env.example .env 
+
+php -v
+
+# php artisan
+# php /var/www/html/artisan migrate:fresh --seed
+
+# composer install 
+
+# docker-docker run --rm npm install
+
+# docker-docker run --rm artisan clear:data
+# docker-docker run --rm artisan cache:clear
+# docker-docker run --rm artisan view:clear
+# docker-docker run --rm artisan route:clear
+# docker-docker run --rm artisan clear-compiled
+# docker-docker run --rm artisan config:cache
+# docker-docker run --rm artisan key:generate
+# docker-docker run --rm artisan storage:link
+# docker-docker run --rm artisan migrate --seed
+# docker-docker run --rm artisan passport:install
